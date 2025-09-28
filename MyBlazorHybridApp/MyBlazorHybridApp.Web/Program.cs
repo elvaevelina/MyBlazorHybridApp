f6e5b88ebfc4020185402f39e9b94225be2ef750
elvaevelina <elvaevelina05@gmail.com>
2025-09-28 23:51:00 +0700

    access data v2

--- MyBlazorHybridApp/MyBlazorHybridApp.Web/Program.cs
+++ MyBlazorHybridApp/MyBlazorHybridApp.Web/Program.cs
@@ -4,9 +4,14 @@ using MyBlazorHybridApp.Web.Services;
 using Microsoft.EntityFrameworkCore;
 using MyBlazorHybridApp.Web.Data;
 using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using BlazingPizza;
+using static System.Net.WebRequestMethods;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -20,6 +25,23 @@ builder.Services.AddDbContext<PizzaStoreContext>(options =>
 
 builder.Services.AddControllers();
 builder.Services.AddHttpClient();
+
+builder.Services.AddScoped<HttpClient>(sp =>
+{
+    var nav = sp.GetRequiredService<NavigationManager>();
+    return new HttpClient { BaseAddress = new Uri(nav.BaseUri) };
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:5173", "https://localhost:7173", "http://10.0.2.2:5173")  // Tambah origins hybrid
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
 
 var app = builder.Build();
 
@@ -35,6 +57,7 @@ else
 }
 
 app.UseHttpsRedirection();
+app.UseCors();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
@@ -42,8 +65,8 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(
-        typeof(MyBlazorHybridApp.Shared._Imports).Assembly,
-        typeof(MyBlazorHybridApp.Web.Client._Imports).Assembly);
+    typeof(MyBlazorHybridApp.Shared._Imports).Assembly);
+
 
 app.MapControllers();
 
@@ -55,3 +78,5 @@ using (var scope = app.Services.CreateScope())
 }
 
 app.Run();
+
+
--- MyBlazorHybridApp/MyBlazorHybridApp/MauiProgram.cs
+++ MyBlazorHybridApp/MyBlazorHybridApp/MauiProgram.cs
@@ -12,11 +12,19 @@ namespace MyBlazorHybridApp
         {
             var builder = MauiApp.CreateBuilder();
 
+            // Dapatkan IP host: Buka CMD di Windows, ipconfig > IPv4 Address (misal 192.168.1.100)
+            // Ganti "192.168.1.100" dengan IP Anda
+            var hostIp = "10.10.10.25";  
+            var port = "5173";  // Gunakan 5173 untuk HTTP, atau 7173 untuk HTTPS
+            var baseUri = DeviceInfo.Platform == DevicePlatform.Android
+                ? $"http://10.0.2.2:{port}/"  // Emulator Android: 10.0.2.2 alias localhost host
+                : DeviceInfo.Platform == DevicePlatform.iOS
+                    ? $"http://localhost:{port}/"  // iOS simulator
+                    : $"http://{hostIp}:{port}/";  // Windows/Desktop atau device fisik (sama WiFi)
             builder.Services.AddScoped(sp => new HttpClient
             {
-                BaseAddress = new Uri(DeviceInfo.Platform == DevicePlatform.Android
-                    ? "http://10.0.2.2:5173/"
-                    : "http://10.10.10.30:5173/") 
+                BaseAddress = new Uri(baseUri),
+                Timeout = TimeSpan.FromSeconds(30)  // Tambah timeout jika lambat
             });
 
             builder
